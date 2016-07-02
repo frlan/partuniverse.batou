b9ee8c31dab8f0d800efa5d1a81515545d81628c
Frank Lanitz <frank@frank.uvena.de>
2016-07-02 16:42:36 +0200

    GitCheckout: fix clone target path

--- components/gitcheckout/component.py
+++ components/gitcheckout/component.py
@@ -7,7 +7,7 @@ class GitCheckout(batou.component.Component):
     git_clone_url = batou.component.Attribute(
         str, 'https://github.com/frlan/partuniverse.git')
     git_revision = 'd2808fc95277cd4884963ec91332cee2fda64114'
-    git_target = 'home/vagrant/clone'
+    git_target = '/home/vagrant/clone'
 
     id_rsa = None
     id_rsa_pub = None
