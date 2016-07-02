8cf6392a264a25c8dea2e3b0c103bb971a6528d2
Frank Lanitz <frank@frank.uvena.de>
2016-07-02 16:50:21 +0200

    PostgreSQL: Add component.
    We are using here postgres from batou_ext instead of implementing the wheel again. It's currently providing what we need

---
+++ components/postgres/component.py
@@ -0,0 +1,19 @@
+import batou
+import batou.component
+import batou.utils
+import batou_ext.postgres
+
+class PostgreSQL(batou.component.Component):
+
+    username = None
+    password = None
+    database = batou.component.Attribute(str, 'partuniverse')
+    command_prefix = batou.component.Attribute(str, 'sudo -u postgres')
+
+    def configure(self):
+        self.address = batou.utils.Address(self.host.fqdn, 5432)
+        #self.provide('postgresql', self)
+
+        self += batou_ext.postgres.User(self.username, password=self.password)
+        self += batou_ext.postgres.DB(self.database, owner=self.username)
+
