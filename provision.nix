28716895e46dc3b1e8db63158d305de6ca7a0bf1
Frank Lanitz <frank@frank.uvena.de>
2016-07-16 14:45:47 +0200

    Remove libjpeg from enviornment as its upstream included now

--- provision.nix
+++ provision.nix
@@ -10,10 +10,6 @@
   flyingcircus.roles.postgresql94.enable = true;
   networking.firewall.enable = false;
 
-  #environment.systemPackages = [
-  #   pkgs.libjpeg
-  #];
-
   # We want that postgres is listen everywhere in vagrant. Default on
   # FCIO nix is ethsrv -- internal network only.
   environment.etc."local/postgresql/9.4/listen.conf".text = ''
